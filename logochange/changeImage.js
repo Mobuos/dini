6e8ae374d6d4fb70b85dcf71a6db24a12052ccbc
João Dini <copperbrjdmdini@gmail.com>
2023-09-06 18:33:18 -0300

    Removes possibility of `changeImage` changing to the same image

--- logochange/changeImage.js
+++ logochange/changeImage.js
@@ -1,8 +1,15 @@
+var previousRandomIndex = -1;
+
 function changeImage() {
     var image = document.getElementById("logo");
-    var randomIndex = Math.floor(Math.random() * 41) + 1;
-    var randomImageName = "/logochange/logos/" + randomIndex + ".png";
-    console.log("AAAAAAAAaa" + randomIndex)
+    var randomIndex;
     
+    do {
+        randomIndex = Math.floor(Math.random() * 41) + 1;
+    } while (randomIndex === previousRandomIndex);
+
+    previousRandomIndex = randomIndex;
+
+    var randomImageName = "/logochange/logos/" + randomIndex + ".png";
     image.src = randomImageName;
 }
