535cf9ee296eb673da93a5c79f43dc4be22df0dd
João Dini <copperbrjdmdini@gmail.com>
2025-07-16 18:50:03 -0300

    Simplifying

--- resources/changeImage.js
+++ resources/changeImage.js
@@ -1,14 +1,14 @@
 var previousRandomIndex = 0;
 
 function changeImage() {
-    var image = document.getElementById("logo");
-    var randomIndex;
-    
-    do {
-        randomIndex = Math.floor(Math.random() * 52) + 1;
-    } while (randomIndex === previousRandomIndex);
-    
-    previousRandomIndex = randomIndex;
-    var randomImageName = "/resources/logos/" + randomIndex + ".jpg";
-    image.src = randomImageName;
+	var image = document.getElementById("logo");
+	var randomIndex;
+
+	do {
+		randomIndex = Math.floor(Math.random() * 38) + 1;
+	} while (randomIndex === previousRandomIndex);
+
+	previousRandomIndex = randomIndex;
+	var randomImageName = "/resources/logos/" + randomIndex + ".jpg";
+	image.src = randomImageName;
 }
